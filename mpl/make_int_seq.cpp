88855cf7031648f8fabc3c7dd8f11e6277980d84
Tony Tang <tangchang21@gmail.com>
2021-03-08 18:35:52 +0800

    demo mpl make_int_seq

---
+++ mpl/make_int_seq.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <array>
+
+template <int... Ns>
+struct int_sequence {
+  using Type = std::array<int, sizeof...(Ns)>;
+  Type data{ { Ns... } };
+  Type make() { return Type(); }
+};
+
+template <int N, int... Ns>
+struct make_int_sequence : make_int_sequence<N - 1, N - 1, Ns...> {};
+
+template <int... Ns>
+struct make_int_sequence<0, Ns...> : int_sequence<Ns...> {};
+
+int main(int argc, char const *argv[]) {
+  auto data = make_int_sequence<3>().data;
+  for (int i = 0; i < data.size(); ++i) {
+    std::cout << data[i] << " ";
+  }
+  return 0;
+}
